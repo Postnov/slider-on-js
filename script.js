5ced3f811a4eed2b179c9893257e4c3223181e40
Postnov Daniil <dan.postnov@gmail.com>
2018-08-14 07:51:08 +0300

    change on constructor function

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 ;document.addEventListener('DOMContentLoaded', function() {
 
+/*
+
 	var slider = document.querySelector('.dp-slider'),
 		wrapper = document.querySelector('.dp-slider__wrapper'),
 		slides = slider.querySelectorAll('.dp-slide'),
@@ -11,61 +13,140 @@
 		nextBtn = document.querySelector('.next'),
 		translateWidth = windowWidth / slidePerPage,
 		initialTranslate = 0;
+*/
 
 
-	//Set width slide and add button in dots list
-	slides.forEach(function(item, i) {
-		item.style.width = (windowWidth / slidePerPage) + 'px';
-		var btn = document.createElement('button');
-		btn.innerText =  ++i;
-		dots.appendChild(btn);
-	});
 
-	slides.forEach(function(item) {
-		slideWidth += item.clientWidth;
-	});
+	function DPSlider (selector, options) {
+		this.selector = document.querySelector(selector) || document.querySelector('.dp-slider');
+		this.options  = options || {};
+
+		var slider           = this.selector,
+			sliderOldContent = this.selector.innerHTML,
+			sliderNewContent = '<div class="dp-slider__wrapper">' + sliderOldContent + '</div>';
+
+		this.selector.innerHTML = sliderNewContent;
 
 
-	slider.style.width = slideWidth + 'px';
+		var wrapperSlider    = slider.querySelector('.dp-slider__wrapper'),
+			slides		     = wrapperSlider.children;
+			slideWidth       = 0,
+			windowWidth      = document.body.clientWidth,
 
-	/* Next Arrow */
+			//opitons
+			slidePerPage     = options.sliderPerPage || 5,
+			dots 		     = options.dots || false,
+			nav		         = options.nav || false,
+			navSpeed         = options.navSpeed || false,
 
-	nextBtn.addEventListener('click', function () {
-		if (initialTranslate < slideWidth - translateWidth) {
-			initialTranslate += translateWidth;
-			wrapper.style.transform = "translateX(-"+ initialTranslate + "px)";
+			translateWidth   = windowWidth / slidePerPage,
+		    initialTranslate = 0;
+		    
+
+
+		for (var i = 0; i < slides.length; i++) {
+
+			//get item width
+			slides[i].style.width = (windowWidth / slidePerPage) + 'px';
+
+			//sliderWidth
+			slideWidth += parseFloat(slides[i].style.width);
 		}
-	});
+
+		wrapperSlider.style.width = slideWidth + 'px';
 
 
-	/* Prev Arrow */
 
-	prevBtn.addEventListener('click', function () {
-		if (initialTranslate.toFixed() === translateWidth.toFixed() ) {
-			wrapper.style.transform = "translateX(0px)";
-			initialTranslate -= translateWidth;
-		}else {
-			initialTranslate -= translateWidth;
-			wrapper.style.transform = "translateX(-"+ initialTranslate + "px)";
+		if (dots === true) {
+			var dotsEl = document.createElement('div');
+			dotsEl.classList.add('dp-slider__dots');
+
+			for (var s = 0; s < slides.length;) {
+				//add dots
+				var dotsBtn = document.createElement('button');
+				dotsBtn.innerText = ++s;
+				dotsEl.appendChild(dotsBtn);
+			}			
+
+			slider.appendChild(dotsEl);
+
+			/* Dots Translate */
+			slider.querySelectorAll('.dp-slider__dots button').forEach(function(item) {
+				item.addEventListener('click', function() {
+					var translateDot = this.innerText * translateWidth;
+					translateDot -= translateWidth;
+					if (translateDot > wrapperSlider.style.width) {
+						wrapperSlider.style.transform = "translateX(-"+ wrapperSlider.style.width +")";
+					}else {
+						wrapperSlider.style.transform = "translateX(-"+ translateDot + "px)";
+					}
+
+					initialTranslate = translateDot;
+				});
+			});			
 		}
-	});
+
+		if (nav === true) {
+			var navEl = document.createElement('div'),
+				prevEl  = document.createElement('button'),
+				nextEl  = document.createElement('button');
+			
+			navEl.classList.add('dp-slider__nav');
+			prevEl.classList.add('dp-slider__prev');
+			nextEl.classList.add('dp-slider__next');
+
+			prevEl.innerHTML = 'prev';
+			nextEl.innerHTML = 'next';
+
+			navEl.appendChild(prevEl);
+			navEl.appendChild(nextEl);
+			slider.appendChild(navEl);
+
+
+			nextEl.addEventListener('click', function () {
+				if (parseFloat(wrapperSlider.style.width) - translateWidth >= initialTranslate.toFixed()) {
+					initialTranslate += translateWidth;
+					wrapperSlider.style.transform = "translateX(-"+ initialTranslate + "px)";
+				}
+			});			
+
+			prevEl.addEventListener('click', function () {
+				if (initialTranslate.toFixed() === translateWidth.toFixed()) {
+					wrapperSlider.style.transform = "translateX(0px)";
+					initialTranslate = 0;
+				}else {
+					if (initialTranslate > 0) initialTranslate -= translateWidth;
+					wrapperSlider.style.transform = "translateX(-"+ initialTranslate + "px)";
+				}
+			});			
+		}
 
 
 
-	/* Dots Translate */
-	document.querySelectorAll('.dots button').forEach(function(item) {
-		item.addEventListener('click', function() {
-			var translateDot = this.innerText * translateWidth;
-			translateDot -= translateWidth;
-			if (translateDot > slider.style.width) {
-				wrapper.style.transform = "translateX(-"+ slider.style.width  + "px)";
-			}else {
-				wrapper.style.transform = "translateX(-"+ translateDot + "px)";
-			}
+	};// end constructor
 
-			initialTranslate = translateDot;
 
-		});
-	});
+
+
+
+
+
+
+
+
+
+	var slider = new DPSlider('.dp-slider', {
+		nav: true,
+		dots: true,
+		slidePerPage: 10
+	})
+
+	var slider2 = new DPSlider('.test-slider', {
+		nav: true,
+		dots: true,
+		slidePerPage: 2
+	})
+
+
 
 });
